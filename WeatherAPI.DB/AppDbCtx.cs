4409aafc514afb72869238b59f7e49784a7a3a6c
Vasily Himchenko <White_Scorpion@inbox.lv>
2024-08-28 00:59:58 +0300

    Initial creation of the DB lib.

---
+++ WeatherAPI.DB/AppDbCtx.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WeatherAPI.DB
+{
+    public class AppDbCtx : DbContext
+    {
+        public AppDbCtx(DbContextOptions<AppDbCtx> options)
+            : base(options)
+        {
+        }
+    }
+}
