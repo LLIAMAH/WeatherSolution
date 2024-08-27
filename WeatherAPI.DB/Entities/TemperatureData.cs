a9b10b994a7dbe7ea44b138b67e57e7d047b6f22
Vasily Himchenko <White_Scorpion@inbox.lv>
2024-08-28 02:04:30 +0300

    Added Temperature data (TEMP)

---
+++ WeatherAPI.DB/Entities/TemperatureData.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WeatherAPI.DB.Entities
+{
+    public class TemperatureData : BaseEntity
+    {
+        public DateTime DateTime { get; set; }
+
+        [ForeignKey(nameof(Country))]
+        public long CountryId { get; set; }
+        public virtual Country Country { get; set; }
+
+        [ForeignKey(nameof(City))]
+        public long CityId { get; set; }
+        public virtual City City { get; set; }
+    }
+}
